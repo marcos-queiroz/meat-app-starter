85bf82e9624117f0473b21fb488865eaeab6b2a5
Marcos Queiroz <marcosmqueiroz@live.com>
2019-08-16 10:37:03 -0300

    Seção 5 - Aula 52

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-
+import { ShoppingCartService } from "./restaurant-detail/shopping-cart/shopping-cart.service";
 
 @NgModule({
   declarations: [
@@ -39,7 +39,8 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
     RouterModule.forRoot(ROUTES)
   ],
   providers: [
-    RestaurantsService
+    RestaurantsService,
+    ShoppingCartService
   ],
   bootstrap: [AppComponent]
 })
